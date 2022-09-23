a0d8eecbfa55058747e73d8b0a209d0ec7ce84ed
gabrielebaglioni <gabrielebaglioni55@gmail.com>
2022-09-23 15:57:42 +0200

    done mancante un'eccezione

---
+++ FileIndirizzi.cs
@@ -0,0 +1,24 @@
+public class Address
+{
+    public string? Name { get; }
+    public string? Surname { get; }
+    public string? Street { get; }
+    public string? City { get; }
+    public string? Province { get; }
+    public string? ZIPCode { get; }
+
+    public Address(string name, string surname, string street, string city, string province, string zipCode)
+    {
+        Name = name;
+        Surname = surname;
+        Street = street;
+        City = city;
+        Province = province;
+        ZIPCode = zipCode;
+    }
+
+    public override string? ToString()
+    {
+        return $"{Name} {Surname}   {Street},{City},{Province} {ZIPCode}";
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿
+string Path = "../../../addresses.csv";
+List<Address> addresses = new List<Address>();
+StreamReader? file = null;
+
+
+try
+{
+    file = File.OpenText(Path);
+    file.ReadLine();
+
+    while (!file.EndOfStream)
+    {
+        string? Line = file.ReadLine();
+        string[] singlePartLine = Line.Split(",");
+
+        if( singlePartLine.Length != 6 && singlePartLine.Length <= 6 )
+        {
+            continue;
+        }
+        string name = singlePartLine[0];
+        string surname = singlePartLine[1];
+        string street = singlePartLine[2];
+        string city = singlePartLine[3];
+        string? province = singlePartLine[4];
+        string zipCode = singlePartLine[5];
+
+        addresses.Add(new Address(name, surname, street, city, province, zipCode));
+    }
+}
+catch (FileNotFoundException)
+{
+    Console.WriteLine("File non trovato");
+}
+finally
+{
+    if (file != null)
+    {
+        foreach (Address address in addresses)
+        {
+            Console.WriteLine(address.ToString());
+        }
+
+    }
+}
